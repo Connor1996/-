69f625d2249313361ae4cf13907a97b871d0db4f
Connor1996 <zbk602423539@gmail.com>
2016-03-09 19:48:46 +0800

    Initial commit

--- program/main.cpp
+++ program/main.cpp
@@ -5,6 +5,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("Travel Query System");
+    w.setFixedSize(1280, 720);
     w.show();
 	//show the window 
     return a.exec();
